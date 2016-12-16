28de3c60cb89542bede51d910ada116fec514842
Marcus Vinícius <marcusviniciusbp@gmail.com>
2016-12-16 18:33:56 -0300

    updated for Ionic 2 RC4; added a new effect (fade -- not yet documented)

--- hsonscroll.ts
+++ hsonscroll.ts
@@ -29,11 +29,13 @@ export class Hsonscroll {
       this.elHeight = this.el.nativeElement.scrollHeight;
       this.elWidth = this.el.nativeElement.scrollWidth;
     }
-
-    if (this.animationType == "slideDown") {
+    if (this.animationType == "fade") {
+      this.el.nativeElement.offsetParent.style.opacity = "0";
+    }
+    if (this.animationType == "fade" || this.animationType == "slideDown") {
+      this.target.resize();
       this.elHeight = this.el.nativeElement.scrollHeight;
       this.initialMarginTop = this.el.nativeElement.style.marginTop;
-      this.target.resize();
       if (!this.relative) {
         this.renderer.setElementStyle(this.el.nativeElement, 'marginTop', "-"+this.elHeight+"px");
       }
@@ -45,11 +47,10 @@ export class Hsonscroll {
   }
 
   ngAfterContentInit() {
-
     if (!this.initialMarginTop) {
       this.initialMarginTop = "0";
     }
-    this.initialScrollTop = this.target.getScrollTop();
+    this.initialScrollTop = this.target.getContentDimensions().scrollTop;
     let positiveScroll: number;
     let negativeScroll: number;
 
@@ -66,62 +67,88 @@ export class Hsonscroll {
       negativeScroll = Math.max(0,this.initialScrollTop-this.hidein);
     }
 
-    this.target.addScrollListener((event)=>{
-      if (this.animationType == "slideDown") {
+    let opacity:number = 0;
+    let increase:number = (1+opacity)/this.showin;
+    this.target.ionScroll.subscribe(($event: any) => {
+      if (this.animationType == "fade") {
         if (!this.relative) {
-          if (this.target.getScrollTop() >= this.showin) {
-            this.renderer.setElementStyle(this.el.nativeElement, 'marginTop', this.initialMarginTop);
+          this.target._scrollEle.style.marginTop = "0";
+          this.target._scrollEle.style.marginTop = "0 !important";
+          this.renderer.setElementStyle(this.el.nativeElement, 'marginTop', this.initialMarginTop);
+          opacity = increase*this.target.getContentDimensions().scrollTop;
+          if (this.target.getContentDimensions().scrollTop >= this.showin) {
+            this.renderer.setElementStyle(this.el.nativeElement, 'opacity', opacity+"");
+            this.el.nativeElement.offsetParent.style.opacity = opacity;
           }
-          if (this.target.getScrollTop() < this.hidein) {
-            this.renderer.setElementStyle(this.el.nativeElement, 'marginTop', "-"+this.elHeight+"px");
-          }
-        } else {
-          if (this.target.getScrollTop() >= positiveScroll) {
-            this.renderer.setElementStyle(this.el.nativeElement, 'marginTop', "-"+this.elHeight+"px");
-            positiveScroll = this.target.getScrollTop()+this.showin;
-            negativeScroll = Math.max(0,this.target.getScrollTop()-this.hidein);
-          } else {
-            if (this.target.getScrollTop() <= negativeScroll) {
-              this.renderer.setElementStyle(this.el.nativeElement, 'marginTop', this.initialMarginTop);
-              positiveScroll = this.target.getScrollTop()+this.showin;
-              negativeScroll = Math.max(0,this.target.getScrollTop()-this.hidein);
-            }
-          }
+          if (this.target.getContentDimensions().scrollTop < this.hidein) {
+            this.renderer.setElementStyle(this.el.nativeElement, 'opacity', opacity+"");
+            this.el.nativeElement.offsetParent.style.opacity = opacity;
 
-          if (this.intervalCtrl == false) {
-            let i = 0;
-            this.intervalCtrl = true;
-            let interval = setInterval(()=>{
-              this.target.resize();
-              i++;
-              if (i == 20) {
-                this.intervalCtrl = false;
-                clearInterval(interval);
-              }
-            }, 25);
           }
         }
       }
+      if (this.animationType == "slideDown") {
+        if (!this.relative) {
+          if (this.target.getContentDimensions().scrollTop >= this.showin) {
+            this.renderer.setElementStyle(this.el.nativeElement, 'marginTop', this.initialMarginTop);
+          }
+          if (this.target.getContentDimensions().scrollTop < this.hidein) {
+            this.renderer.setElementStyle(this.el.nativeElement, 'marginTop', "-"+this.elHeight+"px");
+          }
+        } else {
+          if (this.target.getContentDimensions().scrollTop >= positiveScroll) {
+            this.renderer.setElementStyle(this.el.nativeElement, 'marginTop', "-"+this.elHeight+"px");
+            positiveScroll = this.target.getContentDimensions().scrollTop+this.showin;
+            negativeScroll = Math.max(0,this.target.getContentDimensions().scrollTop-this.hidein);
+          } else {
+            if (this.target.getContentDimensions().scrollTop <= negativeScroll) {
+              this.renderer.setElementStyle(this.el.nativeElement, 'marginTop', this.initialMarginTop);
+              positiveScroll = this.target.getContentDimensions().scrollTop+this.showin;
+              negativeScroll = Math.max(0,this.target.getContentDimensions().scrollTop-this.hidein);
+            }
+          }
+
+          this.contentResizer();
+        }
+      }
+      if (this.animationType == "fabFade") {
+
+      }
       if (this.animationType == "fabFade" && this.relative) {
-        if (this.target.getScrollTop() >= positiveScroll) {
+        if (this.target.getContentDimensions().scrollTop >= positiveScroll) {
           this.renderer.setElementStyle(this.el.nativeElement, 'width', "20px");
           this.renderer.setElementStyle(this.el.nativeElement, 'height', "20px");
           this.renderer.setElementStyle(this.el.nativeElement, 'opacity', "0");
           this.el.nativeElement.firstElementChild.disabled = true;
-          positiveScroll = this.target.getScrollTop()+this.showin;
-          negativeScroll = Math.max(0,this.target.getScrollTop()-this.hidein);
+          positiveScroll = this.target.getContentDimensions().scrollTop+this.showin;
+          negativeScroll = Math.max(0,this.target.getContentDimensions().scrollTop-this.hidein);
         } else {
-          if (this.target.getScrollTop() <= negativeScroll) {
+          if (this.target.getContentDimensions().scrollTop <= negativeScroll) {
             this.renderer.setElementStyle(this.el.nativeElement, 'width', this.elWidth+"px");
             this.renderer.setElementStyle(this.el.nativeElement, 'height', this.elHeight+"px");
             this.renderer.setElementStyle(this.el.nativeElement, 'opacity', "1");
             this.el.nativeElement.firstElementChild.disabled = false;
-            positiveScroll = this.target.getScrollTop()+this.showin;
-            negativeScroll = Math.max(0,this.target.getScrollTop()-this.hidein);
+            positiveScroll = this.target.getContentDimensions().scrollTop+this.showin;
+            negativeScroll = Math.max(0,this.target.getContentDimensions().scrollTop-this.hidein);
           }
         }
       }
     })
+  }
+
+  contentResizer() {
+    if (this.intervalCtrl == false) {
+      let i = 0;
+      this.intervalCtrl = true;
+      let interval = setInterval(()=>{
+        this.target.resize();
+        i++;
+        if (i == 20) {
+          this.intervalCtrl = false;
+          clearInterval(interval);
+        }
+      }, 25);
+    }
   }
 
 }
